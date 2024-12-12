706513b3b40351e037fffdda20188f6d9b085460
stivex001 <stephenadeyemo@gmail.com>
2024-12-12 03:08:17 +0100

    feat:implemented reset password page

--- src/pages/auth/ResetPassword.tsx
+++ src/pages/auth/ResetPassword.tsx
@@ -1,5 +1,112 @@
+import { AuthTitle } from "@/components/authDetails/AuthTitle";
+import AuthLayout from "@/components/layouts/AuthLayout";
+import ControlledInput from "@/components/shared/ControlledInput";
+import CustomButton from "@/components/shared/CustomButton";
+import { Checkbox } from "@/components/ui/checkbox";
+import useDynamicForm from "@/hooks/useDynamicForm";
+import { Field } from "@/schemas/dynamicSchema";
+import { Link } from "react-router-dom";
+
+const fields: Field[] = [
+  {
+    name: "firstname",
+    type: "text",
+    isRequired: true,
+  },
+  {
+    name: "lastname",
+    type: "text",
+    isRequired: true,
+  },
+  {
+    name: "address",
+    type: "text",
+    // isRequired: true,
+  },
+  {
+    name: "phone",
+    type: "text",
+    isRequired: true,
+  },
+
+  {
+    name: "gender",
+    type: "text",
+    // errorMessage: "job title is required",
+    isRequired: true,
+  },
+  {
+    name: "email",
+    type: "email",
+    errorMessage: "Email is required",
+    isRequired: true,
+  },
+  {
+    name: "dob",
+    type: "date",
+    errorMessage: "Date must be selected",
+    // isRequired: true,
+  },
+
+  {
+    name: "password",
+    type: "text",
+    errorMessage: "Password is required",
+    isRequired: true,
+  },
+  {
+    name: "referal",
+    type: "text",
+  },
+];
+
 const ResetPassword = () => {
-  return <div>ResetPassword</div>;
+  const { control, handleSubmit } = useDynamicForm(fields, {});
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <AuthLayout>
+      <div className="flex flex-col gap-10">
+        <AuthTitle
+          title="Reset Password"
+          question="Remember login details ?"
+          link="Log in"
+          url="/auth/login"
+        />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-7"
+        >
+          <ControlledInput
+            name="email"
+            control={control}
+            placeholder="Enter Email"
+            type="email"
+            label="email"
+            variant="primary"
+            rules={{ required: true }}
+          />
+          
+
+
+
+          <div className="flex justify-center">
+            <CustomButton
+              label="Send Password Reset Link"
+              variant="primary"
+              className="w-full"
+              size="lg"
+              type="submit"
+              // disabled={!isValid}
+            />
+          </div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 };
 
 export default ResetPassword;
