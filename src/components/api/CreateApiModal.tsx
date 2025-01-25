c4268b73f2c11388234c4deaf4dea8d886546d2b
stivex001 <stephenadeyemo@gmail.com>
2025-01-25 11:59:08 +0100

    feat:implemented api page

---
+++ src/components/api/CreateApiModal.tsx
@@ -0,0 +1,56 @@
+import { X } from "lucide-react";
+import { ModalHeader } from "../modal/ModalHeader";
+import { DialogContent } from "../ui/dialog";
+import { ModalBody } from "../modal/ModalBody";
+import useDynamicForm from "@/hooks/useDynamicForm";
+import { Field } from "@/schemas/dynamicSchema";
+import CustomButton from "../shared/CustomButton";
+import CustomControlledSelect from "../shared/CustomControlledSelect";
+import { CustomSelect } from "../shared/ControlledSelect";
+
+const fields: Field[] = [
+  {
+    name: "email",
+    type: "email",
+    errorMessage: "Email is required",
+    isRequired: true,
+  },
+];
+
+const showList = [
+  { value: "10", label: "10 days" },
+  { value: "30", label: "30 days" },
+  { value: "60", label: "60 days" },
+];
+
+export const CreateApiModal = () => {
+  const { control, handleSubmit } = useDynamicForm(fields, {});
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <DialogContent className="rounded-[26px] w-[350px]  lg:w-[530px]">
+      <ModalHeader title="Select token validity time" icon={X} />
+      <form  onSubmit={handleSubmit(onSubmit)}>
+        <ModalBody className="border-t border-b py-5">
+          <CustomSelect
+           name="bank"
+           options={showList}
+           control={control}
+           rules={{ required: true }}
+           placeholder="10 days"
+           className="bg-[#F8F9FB] border-none "
+          />
+        </ModalBody>
+        <CustomButton
+          variant="primary"
+          label="Submit"
+          className="w-[129px] h-10 rounded-lg text-xs font-Nunito font-bold mx-auto mt-3"
+          size="lg"
+        />
+      </form>
+    </DialogContent>
+  );
+};
--- src/pages/apiPage/Api.tsx
+++ src/pages/apiPage/Api.tsx
@@ -1,6 +1,8 @@
 import { PageTitle } from "@/components/PageTitle";
 import nodata from "@/assets/images/empty.png";
 import CustomButton from "@/components/shared/CustomButton";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { CreateApiModal } from "@/components/api/CreateApiModal";
 
 const Api = () => {
   return (
@@ -9,17 +11,25 @@ const Api = () => {
       <div className="flex items-center justify-center">
         <div className="space-y-10">
           <img src={nodata} alt="No Data Found" />
-          <h1 className="font-bold text-xl text-center lg:text-4xl">No tokens created</h1>
+          <h1 className="font-bold text-xl text-center lg:text-4xl">
+            No tokens created
+          </h1>
           <div className="flex items-center justify-center gap-3">
+            <Dialog>
+              <DialogTrigger>
+                <CustomButton
+                  label="Create API tokens "
+                  variant="primary"
+                  className="w-fit h-[36px] lg:h-[58px] rounded-[9px] py-4 px-2.5 text-sm lg:text-xl font-medium"
+                  size="lg"
+                  type="button"
+                  // onClick={() => navigate("/sms_campaign/send_sms")}
+                />
+              </DialogTrigger>
+              <CreateApiModal />
+            </Dialog>
+
             <CustomButton
-              label="Create API tokens "
-              variant="primary"
-              className="w-fit h-[36px] lg:h-[58px] rounded-[9px] py-4 px-2.5 text-sm lg:text-xl font-medium"
-              size="lg"
-              type="button"
-              // onClick={() => navigate("/sms_campaign/send_sms")}
-            />
-                        <CustomButton
               label="View API docs "
               variant="primary"
               className="w-fit h-[36px] lg:h-[58px] rounded-[9px] py-4 px-2.5 text-sm lg:text-xl font-medium bg-[#122112]"
