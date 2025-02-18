51858ba9517e004d59e668031c65538873ae2438
stivex001 <stephenadeyemo@gmail.com>
2025-02-18 04:32:06 +0100

    feat:implemented confirm details section

--- src/components/emailCampaign/Design.tsx
+++ src/components/emailCampaign/Design.tsx
@@ -1,5 +1,48 @@
+import { EditIcon } from "@/assets/svgs/MenuIcon";
+import CustomButton from "../shared/CustomButton";
+
 export const Design = () => {
   return (
-    <div className="flex-1 bg-white border border-[#DDDDDD] shadow-lightshadow px-5 py-6"></div>
+    <div className="flex-1 bg-white border border-[#DDDDDD] shadow-lightshadow px-5 py-6">
+      <div className="flex items-center justify-between px-5 mb-7">
+        <h1 className="text-base font-medium ">Design</h1>
+        <CustomButton
+          label="Edit"
+          variant="primary"
+          className="w-fit h-7 rounded-[4px] p-2 text-xs font-medium bg-[#EEEEEE] text-[#555555] hover:bg-[#EEEEEE]"
+          size="lg"
+          type="submit"
+          icon={EditIcon}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="border border-[#DDDDDD] h-[400px]"></div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2.5">
+            <CustomButton
+              label="Preview"
+              variant="primary"
+              className="w-fit h-7 rounded-[4px] p-2 text-xs font-medium bg-[#EEEEEE] text-[#555555] hover:bg-[#EEEEEE]"
+              size="lg"
+              type="button"
+            />
+            <CustomButton
+              label="View plain text version"
+              variant="primary"
+              className="w-fit h-7 rounded-[4px] p-2 text-xs font-medium bg-[#EEEEEE] text-[#555555] hover:bg-[#EEEEEE]"
+              size="lg"
+              type="button"
+            />
+          </div>
+          <CustomButton
+            label="Send test email"
+            variant="primary"
+            className="w-fit h-7 rounded-[4px] p-2 text-xs font-medium bg-[#EEEEEE] text-[#555555] hover:bg-[#EEEEEE]"
+            size="lg"
+            type="button"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
--- src/components/emailCampaign/SubcriberContents.tsx
+++ src/components/emailCampaign/SubcriberContents.tsx
@@ -1,10 +1,22 @@
 import { EditIcon } from "@/assets/svgs/MenuIcon";
 import CustomButton from "../shared/CustomButton";
+import { useSubscribers } from "@/api/crud/subscribers";
+import { ScreenLoader } from "../shared/ScreenLoader";
 
 export const SubcriberContents = () => {
+  const { getSubscriberList } = useSubscribers();
+
+  const { data: list, isPending } = getSubscriberList();
+
+  const groupList = list?.message;
+
+  if (isPending) {
+    return <ScreenLoader />;
+  }
+
   return (
-    <div className="bg-white border border-[#DDDDDD] shadow-lightshadow px-5 py-6">
-      <div className="flex items-center justify-between">
+    <div className="bg-white border border-[#DDDDDD] shadow-lightshadow py-6">
+      <div className="flex items-center justify-between px-5">
         <h1 className="text-base font-medium ">Subscribers</h1>
         <CustomButton
           label="Edit"
@@ -15,12 +27,15 @@ export const SubcriberContents = () => {
           icon={EditIcon}
         />
       </div>
-      <div className="flex flex-col mt-7">
-        <div className="flex items-center justify-between">
+      <div className="flex flex-col mt-7 px-">
+        <div className="flex items-center justify-between border-b border-b-[#DDDDDD] h-7 pb-3 px-5">
           <p className="text-[#555555] text-sm">Sending to</p>
           <p className="text-[#555555] text-sm">Subscribers</p>
         </div>
-        
+        <div className="flex flex-col gap-2 p-5">
+          <p className="text-[#555555] text-sm">Total subscribers</p>
+          <h3 className="text-base font-bold text-black">{groupList?.length}</h3>
+        </div>
       </div>
     </div>
   );
