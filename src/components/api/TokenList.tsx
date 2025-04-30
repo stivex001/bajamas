82afca1e8bd72fb3fa3788a4a4840b0b4fdac712
stivex001 <stephenadeyemo@gmail.com>
2025-04-30 01:04:23 +0100

    feat:integrated copy api token

--- src/components/api/TokenList.tsx
+++ src/components/api/TokenList.tsx
@@ -1,3 +1,4 @@
+import { toast } from "sonner";
 import { ScreenLoader } from "../shared/ScreenLoader";
 
 type Props = {
@@ -9,15 +10,28 @@ const TokenList = ({ token, loading }: Props) => {
   if (loading) {
     return <ScreenLoader />;
   }
-  console.log(token, "token");
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(token);
+      toast.success("Token copied to clipboard");
+    } catch (err) {
+      toast.error("Failed to copy token");
+    }
+  };
 
   return (
     <div className="mt-7">
-      <h1>Token</h1>
+      <h1 className="text-xl text-[#ABA8AA] font-semibold">Token</h1>
       <div className="bg-white rounded-lg shadow-md py-4 px-6 my-7">
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-sm font-semibold text-[#8F8D92]">{token}</h2>
-          <button className="w-fit px-1 py-1.5 bg-[#F5F5F5] text-xs text-[#8E8C8E]">
+          <h2 className="text-sm font-semibold text-[#8F8D92] break-all">
+            {token}
+          </h2>
+          <button
+            onClick={handleCopy}
+            className="w-fit px-2 py-1.5 bg-[#F5F5F5] text-xs text-[#8E8C8E] rounded"
+          >
             Copy Token
           </button>
         </div>
