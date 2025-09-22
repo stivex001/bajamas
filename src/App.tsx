b7d8f757bbb2a5c4b66766e9aa285a0ad75b0f82
stivex001 <stephenadeyemo@gmail.com>
2025-09-22 10:09:09 +0100

    refactor: remove unused shared components and hooks
    - Deleted DetailsCard, Dropdown, EmptyTableState, FilterSelect, Pagination, ProductCard, ReusableDialog, TableLayout, ThumbnailRenderer, VerifyEmailModal components.
    - Removed useCKEditor and useDynamicForm hooks.
    - Eliminated Notifications, ForgotPassword, Login, Register, ResetPassword pages.
    - Cleaned up authStore for better state management.
    - Removed constants related to API URLs and keys.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import { Toaster } from "sonner";
 import LoadingScreen from "./components/shared/ScreenLoader";
 import { lazy, Suspense } from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/Root";
-// import ProtectedRoutes from "./components/layouts/ProtectedRoutes";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import ResetPassword from "./pages/auth/ResetPassword";
 import NotFound from "./pages/NotFound";
-import ForgotPassword from "./pages/auth/ForgotPassword";
-
 
 const Home = lazy(() => import("./pages/landingpage/Home"));
 
@@ -33,7 +24,6 @@ const router = createBrowserRouter([
             element: <Home />,
           },
 
-         
           // {
           //   path: "/api",
           //   element: <Api />,
@@ -44,23 +34,6 @@ const router = createBrowserRouter([
     ],
   },
 
-  {
-    path: "/auth/login",
-    element: <Login />,
-  },
-  {
-    path: "/auth/register",
-    element: <Register />,
-  },
-
-  {
-    path: "/auth/forgot_password",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/auth/reset_password",
-    element: <ResetPassword />,
-  },
   {
     path: "*",
     element: <NotFound />,
